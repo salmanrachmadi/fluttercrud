86e5337c31d1a5bfa625a91451e1294d8a7de948
salmanrachmadi <salmanrachmadi69@gmial.com>
2020-06-17 11:08:47 +0700

    update login,reg

---
+++ lib/Screen/Register.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+class  Register extends StatelessWidget {
+
+  static const routeName = "/register";
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      body: Container(
+        color: Colors.redAccent,
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: <Widget>[
+                  _iconLogin(),
+                  _titleDescription(),
+                  _formField(),
+                  _buttonType(context),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  
+  //Widget iconLogin
+  Widget _iconLogin() {
+    return Image.asset(
+      "assets/images/icon-plane.jpg",
+      width: 150.0,
+      height: 150.0,
+    );
+  }
+   //Widget title Descripti
+  Widget _titleDescription() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        Text(
+          "Register Into ISKY",
+          style: TextStyle(color: Colors.white, fontSize: 15.0),
+          textAlign: TextAlign.center,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+      ],
+    );
+  }
+  //Widget formField
+   Widget _formField() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.blue, 
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.lightBlue,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Username",
+            hintStyle: TextStyle(color: Colors.lightBlue),
+          ),
+          style: TextStyle(color: Colors.black),
+          autofocus: false,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.blue, 
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.lightBlue,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Password",
+            hintStyle: TextStyle(color: Colors.blue),
+          ),
+          style: TextStyle(color: Colors.black),
+          obscureText: true,
+          autofocus: false,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.blue, 
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.lightBlue,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Retype-Password",
+            hintStyle: TextStyle(color: Colors.blue),
+          ),
+          style: TextStyle(color: Colors.black),
+          obscureText: true,
+          autofocus: false,
+        ),
+        
+      ],
+    );
+  }
+  //Widget buttonType
+  Widget _buttonType(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        InkWell(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            width: double.infinity,
+            child: Text(
+              'Register',
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
+          onTap: (){
+
+          },
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        Text(
+          'or',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 12.0,
+          ),
+        ),
+        FlatButton(
+          child: Text(
+            'Login',
+            style: TextStyle(color: Colors.black),
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, "/");
+          },
+        ),
+      ],
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,22 @@
-import 'dart:async';
-import 'dart:convert';
+//import 'dart:async';
+//import 'dart:convert';
+//import 'dart:js';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+//import 'package:http/http.dart' as http;
+import 'package:fluttercrud/Screen/Register.dart';
+import 'package:fluttercrud/Screen/Login.dart';
 
 
-void main() => runApp(MyApp());
-String username='';
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title:'Crud',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        primaryColor: Colors.white
-      ),
-      home: Home(),  
-    );
-  }
-}
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "ISKY",
+    initialRoute: "/",
+    routes: {
+      "/": (context) => Login(),
+      Register.routeName : (context) => Register(),
+    },
+  ));
 }
